fcd2478b1866f2bcb68353266f4d5ffe95a19505
ViniUme <viniciusumetsu210@gmail.com>
2022-05-08 17:07:07 -0300

    add initial files and modified

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default{
+  name: "App",
+  components: { Navbar }
+}
+</script>
+
+<template>
+  <div class="container">
+    <Navbar/>
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+<nav>
+    <ul>
+        <li>Sobre mim</li>
+        <li>Projetos</li>
+        <li>Serviços</li>
+        <li>Minhas Skills</li>
+    </ul>
+</nav>    
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    color: white;
+}
+nav{
+    width: 100vw;
+    height: 100px;
+    background-color: black;
+    text-align: center;
+}
+ul{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+}
+</style>
