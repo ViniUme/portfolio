e6b3e2c4fa8be752f7634bbb8c14927a7eff33e9
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-05-17 15:43:29 -0300

    Edit text about me

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,14 +2,26 @@
     <section class="about">
         <h1 class="about_title">Sobre mim</h1>
         <h2 class="about_text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quas iure dolore! Dolorem veritatis totam aut eos doloribus at eum, repellat aperiam, dignissimos impedit modi illum provident itaque tempora possimus?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam rerum nisi consequatur dolor molestiae vitae itaque quibusdam corporis, optio aspernatur nobis, veritatis dolores quaerat quia mollitia! Necessitatibus ipsam accusamus aspernatur.
+            Sou desenvolvedor Font-End, Back-End e estudante de programação. Tenho {{idade}} anos e estudo programação desde os 13. Busco ser um grande programador e empresário. Logo abaixo você pode ver projetos que eu mesmo fiz para comprovar meu conhecimento na área. Fiz um curso técnico de Desenvolvimento de Sistemas integrado ao ensino médio e foi lá que eu descobri que queria seguir carreira como programador. Gosto muito de tecnologias, matemática, ler, jogar, estudar, e automatizar, foi por isso que eu escolhi essa área. No momento estou focado em aprender refinar meus conehcimentos em React, Node, Next, MongoDB e Typescritp.
         </h2>
     </section>
 </template>
 
 <script>
 export default {
-    name: 'About'
+    name: 'About',
+    data(){
+        return{
+            idade: "'falha ao gerar idade'"
+        }
+    },
+    created(){
+        const data = new Date();
+        const nasc = new Date(2005,0,28,0,0,0,0)
+
+        const idade = data.getFullYear() - nasc.getFullYear();
+        this.idade = idade
+    }
 }
 </script>
 
@@ -23,6 +35,7 @@ export default {
     font-size: 17px;
     margin-top: 10px;
     text-align: justify;
+    line-height: 30px
 }
 .about{
     text-align: center;
