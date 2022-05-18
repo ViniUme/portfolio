68e0157e69cc8a39bcbd12c96e44c92147a837ae
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-05-18 17:03:15 -0300

    Complement about text

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
     <section class="about">
         <h1 class="about_title">Sobre mim</h1>
         <h2 class="about_text">
-            Sou desenvolvedor Font-End, Back-End e estudante de programação. Tenho {{idade}} anos e estudo programação desde os 13. Busco ser um grande programador e empresário. Logo abaixo você pode ver projetos que eu mesmo fiz para comprovar meu conhecimento na área. Fiz um curso técnico de Desenvolvimento de Sistemas integrado ao ensino médio e foi lá que eu descobri que queria seguir carreira como programador. Gosto muito de tecnologias, matemática, ler, jogar, estudar, e automatizar, foi por isso que eu escolhi essa área. No momento estou focado em aprender refinar meus conehcimentos em React, Node, Next, MongoDB e Typescritp.
+            Sou desenvolvedor Font-End, Back-End e estudante de programação. Tenho {{idade}} anos e estudo programação desde os 13. Busco ser um grande programador e empresário. Logo abaixo você pode ver projetos que eu mesmo fiz para comprovar meu conhecimento na área. Fiz um curso técnico de Desenvolvimento de Sistemas integrado ao ensino médio e foi lá que eu descobri que queria seguir carreira como programador. Desde então, comecei a fazer vários cursos voltados à TI. Sou apaixonado por tecnologias, matemática, ler, jogar, estudar, e automatizar, foi por isso que eu escolhi essa área. No momento busco a minha 1º oportunidade para trabalhar como programador junior e estou focado em aprender refinar meus conehcimentos em React, Node, Next, MongoDB e Typescritp.
         </h2>
     </section>
 </template>
@@ -17,7 +17,7 @@ export default {
     },
     created(){
         const data = new Date();
-        const nasc = new Date(2005,0,28,0,0,0,0)
+        const nasc = new Date(2005,0,28)
 
         const idade = data.getFullYear() - nasc.getFullYear();
         this.idade = idade
