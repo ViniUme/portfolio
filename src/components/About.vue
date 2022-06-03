1c6a65ebfd22d62cbefb953ac02da83c2b9712ff
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-03 15:20:28 -0300

    Edit About

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
     <section class="about">
         <h1 class="about_title">Sobre mim</h1>
         <h2 class="about_text">
-            Sou desenvolvedor Font-End, Back-End e estudante de programação. Tenho {{idade}} anos e estudo programação desde os 13. Busco ser um grande programador e empresário. Logo abaixo você pode ver projetos que eu mesmo fiz para comprovar meu conhecimento na área. Fiz um curso técnico de Desenvolvimento de Sistemas integrado ao ensino médio e foi lá que eu descobri que queria seguir carreira como programador. Desde então, comecei a fazer vários cursos voltados à TI. Sou apaixonado por tecnologias, matemática, ler, jogar, estudar, e automatizar, foi por isso que eu escolhi essa área. No momento busco a minha 1º oportunidade para trabalhar como programador junior e estou focado em aprender refinar meus conehcimentos em React, Node, Next, MongoDB e Typescritp.
+            Olá, meu nome é Vinicius Henrique dos Santos, sou desenvolvedor Font-End, Back-End e estudante de programação. Tenho {{idade}} anos e estudo programação desde os 13. Busco ser um grande programador e empresário. Logo abaixo você pode ver projetos que eu mesmo fiz para comprovar meu conhecimento na área. Fiz um curso técnico de Desenvolvimento de Sistemas integrado ao ensino médio e foi lá que eu descobri que queria seguir carreira como programador. Desde então, comecei a fazer vários cursos voltados à TI. Sou apaixonado por tecnologias, matemática, ler, jogar, estudar, e automatizar, foi por isso que eu escolhi essa área. No momento busco a minha 1º oportunidade para trabalhar como programador junior e estou focado em aprender refinar meus conehcimentos em React, Node, Next, MongoDB e Typescritp.
         </h2>
     </section>
 </template>
