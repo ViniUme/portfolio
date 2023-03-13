ddb4f63586fb0d0a0f6bf120eb672b8c9024764a
ViniUme <viniciusumetsu210@gmail.com>
2023-03-13 20:59:32 -0300

    feat: Create contacts section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,11 +41,38 @@ export default function Index(){
                     </div>
                     <Image src="/img/foto.webp" className={styles.self} alt="Minha foto" width={300} height={300}/>
                 </section>
+
                 <section className={styles.about} id="about">
-                    <span className={styles.about_title}>About me</span>
-                    <h2 className={styles.about_text}>
+                    <h2 className={styles.about_title}>About me</h2>
+                    <span className={styles.about_text}>
                         Hi, my name is Vinícius Henrique dos Santos, {age} years old. I'm full-stack developer. Bellow you can see my own projects. I took a technical course in system development and there I discovered my career. I love programming, mathematics, read, play games, study and automate system. At the moment I'm looking to refine my knowledge in the Next.js Framework, Linux and system structure.
-                    </h2>
+                    </span>
+                </section>
+                    
+                <section className={styles.info} id="info">
+                    <h2 className={styles.info_header}>Contacts</h2>
+                    <div className={styles.info_div}>
+                        <a className={styles.info_link} href="mailto:viniciusumetsu210@gmail.com" target="_blank">
+                            <Image className={styles.svg_info} src="/img/icons/email.svg" width={50} height={50} alt="icon_image"/>
+                            <span className={styles.svg_text}>E-mail</span>
+                        </a>
+                        <a className={styles.info_link} href="http://api.whatsapp.com/send?phone=5518997119365" target="_blank">
+                            <Image className={styles.svg_info} src="/img/icons/whatsapp.svg" width={50} height={50} alt="icon_image"/>
+                            <span className={styles.svg_text}>Whatsapp</span>
+                        </a>
+                        <a className={styles.info_link} href="https://www.linkedin.com/in/viniciushenriquesantos/" target="_blank">
+                            <Image className={styles.svg_info} src="/img/icons/linkedin.svg" width={50} height={50} alt="icon_image"/>
+                            <span className={styles.svg_text}>Linkedin</span>
+                        </a>
+                        <a className={styles.info_link} href="https://www.instagram.com/viniciusumetsu/" target="_blank">
+                            <Image className={styles.svg_info} src="/img/icons/instagram.svg" width={50} height={50} alt="icon_image"/>
+                            <span className={styles.svg_text}>Instagram</span>
+                        </a>
+                        <a className={styles.info_link} href="https://t.me/ViniUme" target="_blank">
+                            <Image className={styles.svg_info} src="/img/icons/telegram.svg" width={50} height={50} alt="icon_image"/>
+                            <span className={styles.svg_text}>Telegram</span>
+                        </a>
+                    </div>
                 </section>
             </main>
         </>
