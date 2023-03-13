b121339fcfe06cd78a947d6d4e035940f2074aa7
ViniUme <viniciusumetsu210@gmail.com>
2023-03-13 20:35:50 -0300

    chore: Translate navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,10 +12,10 @@ export default function Navbar(){
     return(
         <nav className={`${styles.nav} ${menu ? styles.activate : null}`}>
             <ul className={styles.menu_list}>
-                <a href="/#about" className={styles.menu_items}>Sobre mim</a>
-                <a href="/#info" className={styles.menu_items}>Contatos</a>
-                <a href="/#projects" className={styles.menu_items}>Projetos</a>
-                <a href="/#skills" className={styles.menu_items}>Minhas Skills</a>
+                <a href="/#about" className={styles.menu_items}>About Me</a>
+                <a href="/#info" className={styles.menu_items}>Contact</a>
+                <a href="/#projects" className={styles.menu_items}>Projects</a>
+                <a href="/#skills" className={styles.menu_items}>My Skills</a>
             </ul>
             <div className={styles.navbar_menu} onClick={SwitchMenu}>
                 <span className={styles.menu_icon}>icon</span>
