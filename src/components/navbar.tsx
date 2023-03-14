a071a6bb12286a6d34b6d03926457b315a6aa6a4
ViniUme <viniciusumetsu210@gmail.com>
2023-03-14 13:51:23 -0300

    fix: Change tag a to Link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
-import styles from '../styles/navbar.module.scss';
 import { useState } from 'react';
+import Link from 'next/link';
+import styles from '../styles/navbar.module.scss';
 
 export default function Navbar(){
 
@@ -12,10 +13,10 @@ export default function Navbar(){
     return(
         <nav className={`${styles.nav} ${menu ? styles.activate : null}`}>
             <ul className={styles.menu_list}>
-                <a href="/#about" className={styles.menu_items}>About Me</a>
-                <a href="/#info" className={styles.menu_items}>Contact</a>
-                <a href="/#projects" className={styles.menu_items}>Projects</a>
-                <a href="/#skills" className={styles.menu_items}>My Skills</a>
+                <Link href="/#about" className={styles.menu_items}>About Me</Link>
+                <Link href="/#info" className={styles.menu_items}>Contact</Link>
+                <Link href="/#projects" className={styles.menu_items}>Projects</Link>
+                <Link href="/#skills" className={styles.menu_items}>My Skills</Link>
             </ul>
             <div className={styles.navbar_menu} onClick={SwitchMenu}>
                 <span className={styles.menu_icon}>icon</span>
