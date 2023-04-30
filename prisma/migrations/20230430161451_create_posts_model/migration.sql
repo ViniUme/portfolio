e64702704794593ca11ac1f8f302d70d5c372425
ViniUme <viniciusumetsu210@gmail.com>
2023-04-30 13:35:11 -0300

    feat: Create migration - create_posts_model

---
+++ prisma/migrations/20230430161451_create_posts_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Posts" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Posts_pkey" PRIMARY KEY ("id")
+);
